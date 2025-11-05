832fc3873ab1b74167379ac524166165312db6f3
iliyasalve <iliyasalve@gmail.com>
2025-11-04 21:13:15 -0500

    Revers modifications

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,47 +1,35 @@
-# Uncomment the imports below before you add the function code
 import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+backend_url = os.getenv('backend_url', default="http://localhost:3030")
+sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
+
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if kwargs:
+        for key, value in kwargs.items():
+            params += f"{key}={value}&"
 
-    request_url = backend_url+endpoint+"?"+params
-
-    print("GET from {} ".format(request_url))
+    request_url = f"{backend_url}{endpoint}?{params.rstrip('&')}" 
+    print("GET from {}".format(request_url))
+    
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
-        # If any error occurs
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"Network exception occurred: {e}")
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url + "analyze/" + text
+    request_url = f"{sentiment_analyzer_url}analyze/{text}"  
+    
     try:
-
         response = requests.get(request_url)
-        
- 
-        response.raise_for_status() 
-
+        response.raise_for_status()  
         json_response = response.json()
         
         if 'sentiment' in json_response:
@@ -60,13 +48,13 @@ def analyze_review_sentiments(text):
         return None 
 
 
-# def post_review(data_dict):
-# Add code for posting review
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = f"{backend_url}/insert_review" 
+    
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"Network exception occurred: {e}")
+
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,21 +6,24 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # Path for registration
+    path(route='register', view=views.registration, name='register'),
 
-    # path for login
+    # Path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
-    path(route='register', view=views.registration, name='register'),
+
+    # Path for getting cars and dealers
     path(route='get_cars', view=views.get_cars, name='getcars'),
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+
+    # Path for getting dealers by state
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
+    # Path for dealer details and reviews
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
+
+    # Path for adding a review
     path(route='add_review', view=views.add_review, name='add_review'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
